d49c45410fe11d8c7f878a028a43e10b2b18202b
justin <justind417@gmail.com>
2019-03-23 10:34:01 -0500

    added pop up menu for color menu and brush size.

--- app/src/main/java/com/oc/Assignment_3/MainActivity.java
+++ app/src/main/java/com/oc/Assignment_3/MainActivity.java
@@ -10,6 +10,11 @@ package com.oc.Assignment_3;
  */
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 public class MainActivity extends Activity {
 
@@ -20,5 +25,40 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         my_canvas = (PaintCanvas) findViewById(R.id.my_canvas);
+
+        /*****Color Picker Menu*****/
+        ImageButton colorPicked = findViewById(R.id.color_picker_btn);
+        final PopupMenu colorPopMenu = new PopupMenu(getApplicationContext(), colorPicked);
+        Menu colorMenu = colorPopMenu.getMenu();
+
+        colorMenu.add(0,0,0, "Red  ");
+        colorMenu.add(0,1,0, "Blue");
+
+        /*****Brush Size Menu*****/
+
+        ImageButton brushSize = findViewById(R.id.brush_size_btn);
+        final PopupMenu brushSizePopMenu = new PopupMenu(getApplicationContext(), brushSize);
+        Menu brushMenu = brushSizePopMenu.getMenu();
+
+        brushMenu.add(0,0,0, "thin");
+        brushMenu.add(0,0,0, "meduim");
+        brushMenu.add(0,0,0, "thick");
+
+
+        /*****On Click Listener for Color Button*****/
+        colorPicked.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                colorPopMenu.show();
+            }
+        });
+
+        /*****On Click Listener for Brush Size Button******/
+        brushSize.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                brushSizePopMenu.show();
+            }
+        });
     }
 }
